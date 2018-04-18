5e956b455ca9b4566e7665f01f774f50390d1d67
Student <Student@BOOK311CZG.win.ftc.ru>
2018-04-18 21:49:39 +0700

    hw4 img

--- app/js/render-template.js
+++ app/js/render-template.js
@@ -1,21 +1,28 @@
+//import {objectsImages} from './objects-images.js';
+
 export function renderTemplate(template, params) 
 {
 	console.log(params);
+	console.log(objectsImages);
 	let regex;
 
 	for (var key in params) {
 	  	if (!Array.isArray(params[key])) {
 			regex = new RegExp('{\%' + key + '\%}', 'g');
 		  	template = template.replace(regex, params[key]);
-	  		continue;
+		  	continue;
 	  	}
 
 		for (var i = 0; i < params[key].length; i++) {
 			regex = new RegExp('{\%' + key + '_' + i + '\%}', 'g');
-		  	template = template.replace(regex, params[key][i]);
-			console.log(params[key][i]);
-		
+		  	template = template.replace(regex, params[key][i]);					
 		}
+
+		// for (let j = 0; j < objectsImages.length; j++) {
+	 //  		if (objectsImages[j].guid == params.guid){
+	 //  		template = template.replace(regex, objectsImages[j].img);
+	 //  		}	
+	 //  	}
 	}
 
 	return template;
--- app/js/script-page-loader.js
+++ app/js/script-page-loader.js
@@ -1,14 +1,13 @@
-import {getSyncJson} from './request-helper.js';
-import {renderTemplate} from './render-template.js';
-import {getTemplate} from './template-lazy-loader.js'
-
-export function loadPage(appGuid, templatePath)
+function loadPage(pageHref) 
 {
-	let appsItemUrl = '/api/apps/' + appGuid + '.json';
-	let objectDescription = getSyncJson(appsItemUrl);
-	let template = getTemplate(templatePath);
-
-	let renderTemplateResult = renderTemplate(template, objectDescription)
-	let container = document.getElementsByClassName("wrapper-section")[0]; 
-	container.innerHTML = renderTemplateResult;
+	let request = new XMLHttpRequest();
+	request.open('GET', pageHref, true);
+	request.onreadystatechange = function() {
+		if ((request.readyState==4) && (request.status==200)) {
+	
+			let container = document.getElementsByClassName("wrapper-section")[0]; 
+			container.innerHTML = request.responseText;
+		}
+	}
+	request.send();
 }
